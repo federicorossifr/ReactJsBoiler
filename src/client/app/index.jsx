93f36b37ca3af69265d82679acc4ab9b17ea4205
Federico Rossi <Federico Rossi>
2016-07-21 16:17:57 +0200

    Initial commit

---
+++ src/client/app/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+var Clicker = React.createClass({
+	getInitialState: function() {
+		return {
+			
+		}
+	},
+	render: function() {
+		return(
+		)
+	}
+
+});
+
+render(<Clicker/>, document.getElementById('app'));
